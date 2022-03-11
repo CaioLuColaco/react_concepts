01fe918304a8a6ca0c95154d7db49948dca87c0e
caio lucena <caio.lcolaco@gmail.com>
2022-03-11 14:55:30 -0300

    nomarlabs version

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,6 +4,7 @@ export function Header() {
   return (
     <header className="header">
       <div>
+        <h1>NormaLabs</h1>
         <img src="/logo.svg" alt="to.do"/>
         {/* <img src="/fotoPerfil.png" alt="to.do"/> */}
       </div>
